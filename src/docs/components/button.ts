71405b8ff059c3765c30452b09bccefe09270c20
printf83 <printf83@gmail.com>
2023-04-21 21:37:24 +0800

    update button

--- src/docs/components/button.ts
+++ src/docs/components/button.ts
@@ -27,7 +27,7 @@ export const button: IAttrContent = {
 		new e.alert({ color: "info", callout: true }, [
 			new h.h(5, "Conveying meaning to assistive technologies"),
 			new h.p(
-				"Using color to add meaning only provides a visual indication, which will not be conveyed to users of assistive technologies – such as screen readers. Ensure that information denoted by the color is either obvious from the content itself (e.g. the visible text), or is included through alternative means, such as additional text hidden with the {{.visually-hidden}} class."
+				"Using color to add meaning only provides a visual indication, which will not be conveyed to users of assistive technologies – such as screen readers. Ensure that information denoted by the color is either obvious from the content itself (e.g. the visible text), or is included through alternative means, such as additional text hidden with the {{visually:'hidden'}} property."
 			),
 		]),
 
@@ -35,17 +35,20 @@ export const button: IAttrContent = {
 
 		new e.title("Disable text wrapping"),
 		new e.text(
-			"If you don’t want the button text to wrap, you can add the {{.text-nowrap}} class to the button. In Sass, you can set {{$btn-white-space: nowrap}} to disable text wrapping for each button."
+			"If you don’t want the button text to wrap, you can add the {{textWrap:false}} class to the button. In Sass, you can set {{$btn-white-space: nowrap}} to disable text wrapping for each button."
 		),
 
 		//-----------------------
 
 		new e.title("Button tags"),
 		new e.text(
-			"The {{.btn}} classes are designed to be used with the {{<button>}} element. However, you can also use these classes on {{<a>}} or {{<input>}} elements (though some browsers may apply a slightly different rendering)."
+			"{{b.button}} component automaticly add {{.btn}} class and retun a {{h.button}} component. If you provide {{href:<href>}} to the component, it will return {{h.a}} component."
 		),
 		new e.text(
-			"When using button classes on {{<a>}} elements that are used to trigger in-page functionality (like collapsing content), rather than linking to new pages or sections within the current page, these links should be given a {{role='button'}} to appropriately convey their purpose to assistive technologies such as screen readers."
+			"The {{.btn}} classes are designed to be used with the {{h.button}} component. However, you can also use these classes on {{h.a}} or {{h.input}} component (though some browsers may apply a slightly different rendering)."
+		),
+		new e.text(
+			"When using button classes on {{h.a}} component that are used to trigger in-page functionality (like collapsing content), rather than linking to new pages or sections within the current page, these links should be given a {{role:'button'}} to appropriately convey their purpose to assistive technologies such as screen readers."
 		),
 		new e.code({
 			outputAttr: { gap: 1 },
@@ -68,7 +71,7 @@ export const button: IAttrContent = {
 
 		new e.title("Outline buttons"),
 		new e.text(
-			"In need of a button, but not the hefty background colors they bring? Replace the default modifier classes with the {{.btn-outline-*}} ones to remove all background images and colors on any button."
+			"In need of a button, but not the hefty background colors they bring? Replace the default modifier classes with the {{outline:true}} ones to remove all background images and colors on any {{b.button}}."
 		),
 		new e.code({
 			outputAttr: { gap: 1 },
@@ -85,13 +88,13 @@ export const button: IAttrContent = {
 		}),
 		new e.alert(
 			{ color: "info", callout: true },
-			" Some of the button styles use a relatively light foreground color, and should only be used on a dark background in order to have sufficient contrast."
+			"Some of the button styles use a relatively light foreground color, and should only be used on a dark background in order to have sufficient contrast."
 		),
 
 		//-----------------------
 
 		new e.title("Sizes"),
-		new e.text("Fancy larger or smaller buttons? Add {{.btn-lg}} or {{.btn-sm}} for additional sizes."),
+		new e.text("Fancy larger or smaller buttons? Add {{weight:'lg'|'sm'}} for additional sizes."),
 		new e.code({
 			outputAttr: { gap: 1 },
 			output: () => {
@@ -131,7 +134,7 @@ export const button: IAttrContent = {
 
 		new e.title("Disabled state"),
 		new e.text(
-			"Make buttons look inactive by adding the {{disabled}} boolean attribute to any {{<button>}} element. Disabled buttons have {{pointer-events: none}} applied to, preventing hover and active states from triggering."
+			"Make buttons look inactive by adding the {{disabled:true}} property to any {{h.button}} component. Disabled buttons have {{pointer-events: none}} applied to, preventing hover and active states from triggering."
 		),
 		new e.code({
 			outputAttr: { gap: 1 },
@@ -153,6 +156,7 @@ export const button: IAttrContent = {
 				"Disabled buttons using {{<a>}} should not include the {{href}} attribute.",
 			],
 		}),
+		new e.text("Bootstrap TS handle this if you using {{b.button({href:'#'})}} component"),
 		new e.code({
 			outputAttr: { gap: 1 },
 			output: () => {
@@ -166,8 +170,9 @@ export const button: IAttrContent = {
 
 		new e.subtitle("Link functionality caveat"),
 		new e.text(
-			"To cover cases where you have to keep the {{href}} attribute on a disabled link, the {{.disabled}} class uses {{pointer-events: none}} to try to disable the link functionality of {{<a>}}s. Note that this CSS property is not yet standardized for HTML, but all modern browsers support it. In addition, even in browsers that do support {{pointer-events: none}}, keyboard navigation remains unaffected, meaning that sighted keyboard users and users of assistive technologies will still be able to activate these links. So to be safe, in addition to {{aria-disabled='true'}}, also include a {{tabindex='-1'}} attribute on these links to prevent them from receiving keyboard focus, and use custom JavaScript to disable their functionality altogether."
+			"To cover cases where you have to keep the {{href}} property on a disabled link, the {{disabled:true}} property uses {{pointer-events: none}} to try to disable the link functionality of {{h.a}}s. Note that this CSS property is not yet standardized for HTML, but all modern browsers support it. In addition, even in browsers that do support {{pointer-events: none}}, keyboard navigation remains unaffected, meaning that sighted keyboard users and users of assistive technologies will still be able to activate these links. So to be safe, in addition to {{aria-disabled='true'}}, also include a {{tabindex='-1'}} attribute on these links to prevent them from receiving keyboard focus, and use custom JavaScript to disable their functionality altogether."
 		),
+		new e.text("Bootstrap TS handle this automaticly."),
 		new e.code({
 			outputAttr: { gap: 1 },
 			output: () => {
@@ -190,7 +195,7 @@ export const button: IAttrContent = {
 			},
 		}),
 		new e.text(
-			"Here we create a responsive variation, starting with vertically stacked buttons until the {{md}} breakpoint, where {{.d-md-block}} replaces the {{.d-grid}} class, thus nullifying the {{gap-2}} utility. Resize your browser to see them change."
+			"Here we create a responsive variation, starting with vertically stacked buttons until the {{md}} breakpoint, where {{display:'md-block'}} replaces the {{display:'grid'}}, thus nullifying the {{gap:2}} utility. Resize your browser to see them change."
 		),
 		new e.code({
 			showViewport: true,
@@ -202,7 +207,7 @@ export const button: IAttrContent = {
 			},
 		}),
 		new e.text(
-			"You can adjust the width of your block buttons with grid column width classes. For example, for a half-width “block button”, use {{.col-6}}. Center it horizontally with {{.mx-auto}}, too."
+			"You can adjust the width of your block buttons with grid column width classes. For example, for a half-width “block button”, use {{col:6}}. Center it horizontally with {{marginX:'auto'}}, too."
 		),
 		new e.code({
 			output: () => {
@@ -237,7 +242,7 @@ export const button: IAttrContent = {
 
 		new e.subtitle("Toggle states"),
 		new e.text(
-			"Add {{data-bs-toggle='button'}} to toggle a button’s {{active}} state. If you’re pre-toggling a button, you must manually add the {{.active}} class and {{aria-pressed='true'}} to ensure that it is conveyed appropriately to assistive technologies."
+			"Add {{toggle:true}} to toggle a button’s {{active}} state. If you’re pre-toggling a button, you must manually add the {{active:true}} property. The {{aria-pressed='true'}} add automaticly by Bootstrap TS to ensure that it is conveyed appropriately to assistive technologies."
 		),
 		new e.code({
 			outputAttr: { gap: 1 },
