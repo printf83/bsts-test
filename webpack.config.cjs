5f2327aac04b84b8db10228dff152b79d5a4d3d6
printf83 <printf83@gmail.com>
2023-07-09 16:51:17 +0800

    update

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require("path");
 const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
-	mode: "development",
+	mode: "production",
 	entry: "./src/index.ts",
 	plugins: [new CompressionPlugin({ exclude: /\/docs/ })],
 	module: {
