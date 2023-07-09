985b892c965e93c758c61708ed3cf201c631a7e3
printf83 <printf83@gmail.com>
2023-07-09 16:35:42 +0800

    update

--- build/ctl/main/_index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.genMain = exports.genMainContent = exports.container = exports.Container = void 0;
-const container_js_1 = require("./container.js");
-Object.defineProperty(exports, "container", { enumerable: true, get: function () { return container_js_1.container; } });
-Object.defineProperty(exports, "genMainContent", { enumerable: true, get: function () { return container_js_1.genMainContent; } });
-Object.defineProperty(exports, "genMain", { enumerable: true, get: function () { return container_js_1.genMain; } });
-Object.defineProperty(exports, "Container", { enumerable: true, get: function () { return container_js_1.Container; } });
