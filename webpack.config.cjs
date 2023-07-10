7d152ccfa852c14f9a54fa186a2fb7cc4c1f700c
printf83 <printf83@gmail.com>
2023-07-10 10:08:59 +0800

    gh-pages complete

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,39 +1,25 @@
 const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = [
-	{
-		mode: "development",
-		plugins: [
-			new CopyWebpackPlugin({
-				patterns: [{ from: "static" }],
-			}),
-		],
-		output: {
-			path: path.resolve(__dirname, "./dist"),
-		},
-	},
-	{
-		mode: "development",
-		entry: "./src/index.ts",
-		module: {
-			rules: [
-				{
-					test: /\.ts$/,
-					use: "ts-loader",
-				},
-			],
-		},
-		resolve: {
-			extensionAlias: {
-				".js": [".ts", ".js"],
+module.exports = {
+	mode: "development",
+	entry: "./src/index.ts",
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
 			},
-			modules: ["node_modules"],
-			extensions: [".ts", ".js", ".json"],
-		},
-		output: {
-			filename: "[name].js",
-			path: path.resolve(__dirname, "./dist/src"),
-		},
+		],
 	},
-];
+	resolve: {
+		extensionAlias: {
+			".js": [".ts", ".js"],
+		},
+		modules: ["node_modules"],
+		extensions: [".ts", ".js", ".json"],
+	},
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "./dist/src"),
+	},
+};
