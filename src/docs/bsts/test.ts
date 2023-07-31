9014da66cfc2fa432e7f81c3628ce107e1f3e2d5
Hamzah Aziezs <printf83@gmail.com>
2023-07-31 17:44:40 +0800

    accent color test

--- src/docs/bsts/test.ts
+++ src/docs/bsts/test.ts
@@ -1,6 +1,100 @@
 import { IContent, getContentCode, resetContentIndex } from "../../ctl/main/content.js";
 import * as e from "../../ctl/example/_index.js";
-import { b, h } from "@printf83/bsts";
+import { b, core, h } from "@printf83/bsts";
+
+interface variableItem {
+	variableName: string;
+	selector?: string;
+	value?: string;
+}
+
+const CUSTOMCSSVARDB: variableItem[] = [];
+
+const addCustomCSSVar = (variableName: string, value: string, selector?: string) => {
+	//find index
+	const index = CUSTOMCSSVARDB.findIndex((i) => {
+		return i.variableName === variableName && i.selector === selector;
+	});
+
+	//add, remove or change value
+	if (index > -1) {
+		if (value) {
+			CUSTOMCSSVARDB[index]!.value = value;
+		} else {
+			CUSTOMCSSVARDB.splice(index, 1);
+		}
+	} else {
+		if (value) {
+			CUSTOMCSSVARDB.push({
+				variableName: variableName,
+				selector: selector,
+				value: value,
+			});
+		}
+	}
+};
+
+const setCustomCSSVar = () => {
+	//sort
+	CUSTOMCSSVARDB.sort((a, b) =>
+		`${a.selector}${a.variableName}` > `${b.selector}${b.variableName}` ? -1 : 1
+	);
+
+	//gen css
+	const CSS: string[] = [];
+	let lastSelector: string | null = null;
+
+	CUSTOMCSSVARDB.forEach((i) => {
+		const sel = i.selector ? i.selector : ":root";
+
+		if (lastSelector !== sel) {
+			if (lastSelector !== null) {
+				CSS.push("}\n");
+			}
+
+			CSS.push(`${sel} {\n`);
+			lastSelector = sel;
+		}
+
+		CSS.push(`\t${i.variableName}: ${i.value};\n`);
+	});
+
+	if (CSS && CSS.length > 0) {
+		CSS.push("}\n");
+	}
+
+	//add to custom stylesheet
+	let bstsCustomVarContainer = document.getElementById("bsts-custom-var-container");
+	if (!bstsCustomVarContainer) {
+		core.appendChild(
+			document.head,
+			new h.style({ id: "bsts-custom-var-container" }, CSS.join(""))
+		);
+	} else {
+		core.replaceWith(
+			bstsCustomVarContainer,
+			new h.style({ id: "bsts-custom-var-container" }, CSS.join(""))
+		);
+	}
+};
+
+const setupCustomCSSVar = (hex: string) => {
+	const rgb = core.hexToRGB(hex);
+	const sRGB = `${rgb?.r},${rgb?.g},${rgb?.b}`;
+
+	addCustomCSSVar("--bs-primary", hex);
+	addCustomCSSVar("--bs-primary-rgb", sRGB);
+
+	addCustomCSSVar("--bs-dropdown-link-active-bg", hex, ".dropdown-item");
+
+	addCustomCSSVar("--bs-list-group-active-bg", hex, ".list-group");
+	addCustomCSSVar("--bs-list-group-active-border-color", hex, ".list-group");
+
+	addCustomCSSVar("--bs-btn-bg", hex, ".btn-primary");
+	addCustomCSSVar("--bs-btn-border-color", hex, ".btn-primary");
+
+	setCustomCSSVar();
+};
 
 export const test: IContent = {
 	title: "Testing",
@@ -8,7 +102,52 @@ export const test: IContent = {
 	item: (db?: e.ISourceDB[]) => {
 		resetContentIndex();
 
+		const cssBsPrimary = core.getCSSVarHexColor("--bs-primary");
+
 		return [
+			//----------------------
+
+			new e.section([
+				new e.title("Accent color"),
+				new e.code({
+					db: getContentCode(db),
+					outputAttr: { display: "flex", gap: 2 },
+					output: () => {
+						return new h.div(
+							{
+								class: "swatch",
+								rounded: true,
+								border: true,
+								style: { backgroundColor: cssBsPrimary },
+							},
+							[
+								new b.input({
+									type: "color",
+									value: cssBsPrimary,
+									on: {
+										input: (e) => {
+											let target = e.target as HTMLInputElement;
+											let value = target.value;
+											let container = target.closest(
+												".swatch"
+											) as HTMLElement;
+
+											if (container) {
+												container.style.setProperty(
+													"background-color",
+													value
+												);
+												setupCustomCSSVar(value);
+											}
+										},
+									},
+								}),
+							]
+						);
+					},
+				}),
+			]),
+
 			//----------------------
 
 			new e.section([
