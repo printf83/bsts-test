f1f7124de2202a133c6444e194609c23f7046364
Hamzah Aziezs <printf83@gmail.com>
2023-08-09 11:59:14 +0800

    update

--- src/ctl/main/_db.ts
+++ src/ctl/main/_db.ts
@@ -1,7 +1,7 @@
 import { IMenu, IMenuItem } from "./menu.js";
 
 export const DEFAULTDOCUMENT = "docs/gettingstarted/home";
-export const CURRENTVERSION = "0.3.10";
+export const CURRENTVERSION = "0.3.11";
 
 export const THEMEDB = [
 	{
