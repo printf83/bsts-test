1589907b7963e37bfc410025489a54d8af6035aa
Hamzah Aziezs <printf83@gmail.com>
2023-09-18 17:07:28 +0800

    carousel example complete

--- src/docs/example/carousel.ts
+++ src/docs/example/carousel.ts
@@ -105,9 +105,9 @@ export const carousel: IContent = {
 			description: string;
 			href: string;
 		}) => {
-			return new h.div({ col: "lg-4", textAlign: "center", marginBottom: 3, padding: 2 }, [
+			return new h.div({ col: "lg-4", textAlign: "center", marginY: 3, padding: 2 }, [
 				new b.img({ rounded: "circle", src: opt.src }),
-				new h.h(2, { fontWeight: "normal" }, opt.title),
+				new h.h(2, { fontWeight: "normal", margin: 2 }, opt.title),
 				new h.p(opt.description),
 				new h.p(new b.button({ color: "secondary", href: opt.href }, "View detail")),
 			]);
@@ -143,54 +143,84 @@ export const carousel: IContent = {
 			)
 		);
 
-		const heading2_item = (opt: {
+		interface heading2_item_option {
+			img_location: "start" | "end";
 			src: string;
 			title: string;
+			subtitle: string;
 			description: string;
 			href: string;
-		}) => {
-			return new h.div({ container: true }, [
-				new h.hr({ marginY: 5 }),
-				new h.div({ row: true }, [new h.div({ col: "md-7" }), new h.div({ col: "md-5" })]),
-				// new h.div({ col: "lg-4", textAlign: "center", marginBottom: 3, padding: 2 }, [
-				// 	new b.img({ rounded: "circle", src: opt.src }),
-				// 	new h.h(2, { fontWeight: "normal" }, opt.title),
-				// 	new h.p(opt.description),
-				// 	new h.p(new b.button({ color: "secondary", href: opt.href }, "View detail")),
-				// ]),
+		}
+
+		const heading2_item = (opt: heading2_item_option) => {
+			const picture = new h.div({ col: "md-5" }, new b.img({ fluid: true, src: opt.src }));
+			const detail = new h.div({ col: "md-7" }, [
+				new h.h(1, { fontWeight: "normal", lineHeight: 1, marginTop: 5 }, [
+					opt.title,
+					" ",
+					new h.span({ textColor: "body-secondary" }, opt.subtitle),
+				]),
+				new h.p({ lead: true }, opt.description),
 			]);
+			const container =
+				opt.img_location === "start"
+					? new h.div({ row: true }, [picture, detail])
+					: new h.div({ row: true }, [detail, picture]);
+
+			return new h.div({ container: true }, [new h.hr({ marginY: 5 }), container]);
 		};
 
 		const heading2 = new h.div(
-			{ container: true, marginY: 5 },
+			{ container: true, marginY: 5, paddingY: 3 },
 			new h.div(
 				{ row: true },
 				[
 					{
-						src: "https://picsum.photos/seed/bsts_3/140/140.webp",
-						title: "Heading",
+						img_location: "end",
+						src: "https://picsum.photos/seed/bsts_6/500/500.webp",
+						title: "First featurette heading.",
+						subtitle: "It’ll blow your mind.",
 						description:
-							"Some representative placeholder content for the three columns of text below the carousel. This is the first column.",
+							"Some great placeholder content for the first featurette here. Imagine some exciting prose here.",
 						href: "#",
 					},
 					{
-						src: "https://picsum.photos/seed/bsts_4/140/140.webp",
-						title: "Heading",
+						img_location: "start",
+						src: "https://picsum.photos/seed/bsts_7/500/500.webp",
+						title: "Oh yeah, it’s that good.",
+						subtitle: "See for yourself.",
 						description:
-							"Another exciting bit of representative placeholder content. This time, we've moved on to the second column.",
+							"Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.",
 						href: "#",
 					},
 					{
-						src: "https://picsum.photos/seed/bsts_5/140/140.webp",
-						title: "Heading",
+						img_location: "end",
+						src: "https://picsum.photos/seed/bsts_8/500/500.webp",
+						title: "And lastly, this one.",
+						subtitle: "Checkmate.",
 						description:
-							"And lastly this, the third column of representative placeholder content.",
+							"And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.",
 						href: "#",
 					},
-				].map((i) => heading2_item(i))
+				].map((i) => heading2_item(i as heading2_item_option))
 			)
 		);
 
-		return [header(), slider(), heading, heading2, new h.div("content")];
+		const footer = new h.footer(
+			new h.div({ container: true }, [
+				new h.hr({ marginY: 5 }),
+				new h.div([
+					new h.p({ float: "end" }, new h.a({ href: "#" }, "Back to top")),
+					new h.p({ marginBottom: 5 }, [
+						"© 2017–2023 Company, Inc. · ",
+						new h.a({ href: "#" }, "Privacy"),
+						" · ",
+						new h.a({ href: "#" }, "Term"),
+					]),
+				]),
+			])
+		);
+
+		return [header(), slider(), heading, heading2, footer];
 	},
 };
