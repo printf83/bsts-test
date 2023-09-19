7264565897a233f37fc8ad30bd8813ce53339c86
Hamzah Aziezs <printf83@gmail.com>
2023-09-19 16:15:35 +0800

    update

--- src/docs/example/dashboard.ts
+++ src/docs/example/dashboard.ts
@@ -6,73 +6,84 @@ export const dashboard: IContent = {
 	title: "Dashboard",
 	item: () => {
 		const header = new b.navbar.container(
-			{ bgColor: "body-tertiary", fixed: "top" },
+			{
+				theme: "dark",
+				bgColor: "dark",
+				fixed: "top",
+				expand: "md",
+				sticky: "top",
+				padding: 0,
+				flex: "md-nowrap",
+				shadow: true,
+			},
 			new b.navbar.innercontainer({ container: "fluid" }, [
-				new b.navbar.brand({ href: "#" }, "Offcanvas navbar"),
+				new b.navbar.brand({ href: "#" }, "Company name"),
 				new b.navbar.toggle.offcanvas({
 					target: "#offcanvasNavbar",
 					controlfor: "offcanvasNavbar",
 					label: "Toggle navigation",
 				}),
-				new b.offcanvas.container(
-					{
-						id: "offcanvasNavbar",
-						placement: "end",
-						labelledby: "offcanvasNavbarLabel",
-					},
-					[
-						new b.offcanvas.header([
-							new b.offcanvas.title({ id: "offcanvasNavbarLabel" }, "Offcanvas"),
-							new b.offcanvas.btnclose(),
-						]),
-						new b.offcanvas.body([
-							new b.navbar.itemcontainer(
-								{
-									justifyContent: "end",
-									flex: "grow-1",
-									paddingEnd: 3,
-								},
-								[
-									new b.navbar.item(
-										new b.navbar.link({ href: "#", active: true }, "Home")
-									),
-									new b.navbar.item(new b.navbar.link({ href: "#" }, "Link")),
-									new b.navbar.item({ dropdown: true }, [
-										new b.dropdown.button({ navItem: true }, "Dropdown"),
-										new b.dropdown.menu([
-											new b.dropdown.item({ href: "#" }, "Action"),
-											new b.dropdown.item({ href: "#" }, "Another action"),
-											new b.dropdown.divider(),
-											new b.dropdown.item(
-												{ href: "#" },
-												"Something else here"
-											),
-										]),
-									]),
-								]
-							),
-							new h.form({ display: "flex", role: "search", marginTop: 3 }, [
-								new b.input({
-									type: "search",
-									marginEnd: 2,
-									placeholder: "Search",
-									label: "Search",
-								}),
-								new b.button(
-									{
-										type: "submit",
-										color: "success",
-										outline: true,
-									},
-									"Search"
-								),
-							]),
-						]),
-					]
-				),
 			])
 		);
 
-		return [header];
+		const sidebar = new h.div(
+			{ border: "end", col: ["md-3", "lg-2"], padding: 0, bgColor: "body-tertiary" },
+			new b.offcanvas.container(
+				{
+					id: "offcanvasNavbar",
+					placement: "end",
+					show: "md",
+					bgColor: "body-tertiary",
+					labelledby: "offcanvasNavbarLabel",
+				},
+				[
+					new b.offcanvas.header([
+						new b.offcanvas.title({ id: "offcanvasNavbarLabel" }, "Company name"),
+						new b.offcanvas.btnclose(),
+					]),
+					new b.offcanvas.body(
+						{
+							display: "md-flex",
+							flex: "column",
+							// padding: 0,
+							paddingTop: "lg-3",
+							overflowY: "auto",
+						},
+						[
+							new b.navbar.itemcontainer(
+								{
+									flex: "column",
+									marginBottom: "auto",
+								},
+								[
+									{ icon: "house", label: "Dashboard", href: "#" },
+									{ icon: "file-earmark", label: "Orders", href: "#" },
+									{ icon: "cart", label: "Products", href: "#" },
+									{ icon: "people", label: "Customers", href: "#" },
+									{ icon: "graph-up", label: "Report", href: "#" },
+									{ icon: "puzzle", label: "Integrations", href: "#" },
+								].map((i) => {
+									return new b.navbar.item(
+										new b.navbar.link(
+											{ href: i.href, paddingX: 3 },
+											new b.caption({ icon: new b.icon(i.icon) }, i.label)
+										)
+									);
+								})
+							),
+						]
+					),
+				]
+			)
+		);
+
+		const main = new h.main(
+			{ col: ["md-9", "lg-10"], marginStart: "sm-auto", paddingX: "md-4" },
+			"main"
+		);
+
+		const middle = new h.div({ container: "fluid" }, new h.div({ row: true }, [sidebar, main]));
+
+		return [header, middle];
 	},
 };
