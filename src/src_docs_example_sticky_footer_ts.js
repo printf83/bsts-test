54a44b009d9d5e368000572fa4d4c85475fbb116
printf83 <printf83@gmail.com>
2023-09-20 21:46:37 +0800

    Updates

---
+++ src/src_docs_example_sticky_footer_ts.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkbsts_test"] = self["webpackChunkbsts_test"] || []).push([["src_docs_example_sticky_footer_ts"],{
+
+/***/ "./src/docs/example/sticky_footer.ts":
+/*!*******************************************!*\
+  !*** ./src/docs/example/sticky_footer.ts ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   sticky_footer: () => (/* binding */ sticky_footer)\n/* harmony export */ });\n/* harmony import */ var _printf83_bsts__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @printf83/bsts */ \"../bsts/build/esm/index.js\");\n\nconst sticky_footer = {\n    fullscreen: true,\n    title: \"Sticky Footer\",\n    item: () => {\n        const main = new _printf83_bsts__WEBPACK_IMPORTED_MODULE_0__.h.main({ flex: \"shrink-0\" }, new _printf83_bsts__WEBPACK_IMPORTED_MODULE_0__.h.div({ container: true }, [\n            new _printf83_bsts__WEBPACK_IMPORTED_MODULE_0__.h.h(1, { marginTop: 5 }, \"Sticky footer\"),\n            new _printf83_bsts__WEBPACK_IMPORTED_MODULE_0__.h.p({ lead: true }, \"Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.\"),\n            new _printf83_bsts__WEBPACK_IMPORTED_MODULE_0__.h.p(\"Use {{docs/5.3/examples/sticky-footer/::the sticky footer with a fixed navbar}} if need be, too.\"),\n        ]));\n        const footer = new _printf83_bsts__WEBPACK_IMPORTED_MODULE_0__.h.footer({ sticky: \"bottom\", marginTop: \"auto\", paddingY: 3, bgColor: \"body-tertiary\" }, new _printf83_bsts__WEBPACK_IMPORTED_MODULE_0__.h.div({ container: true }, new _printf83_bsts__WEBPACK_IMPORTED_MODULE_0__.h.span({ textColor: \"body-secondary\" }, \"Place sticky footer content here.\")));\n        return new _printf83_bsts__WEBPACK_IMPORTED_MODULE_0__.h.div({ display: \"flex\", flex: \"column\", viewHeight: 100 }, [main, footer]);\n    },\n};\n\n\n//# sourceURL=webpack://bsts-test/./src/docs/example/sticky_footer.ts?");
+
+/***/ })
+
+}]);
