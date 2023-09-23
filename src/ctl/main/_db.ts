8c9f78d400951a74023f571819ffc33860200fae
printf83 <printf83@gmail.com>
2023-09-24 01:03:16 +0800

    update bsts version to 1.0.1

--- src/ctl/main/_db.ts
+++ src/ctl/main/_db.ts
@@ -1,7 +1,7 @@
 import { IMenu, IMenuItem } from "./menu.js";
 
 export const DEFAULTDOCUMENT = "docs/gettingstarted/home";
-export const CURRENTVERSION = "1.0.0";
+export const CURRENTVERSION = "1.0.1";
 
 export const THEMEDB = [
 	{
